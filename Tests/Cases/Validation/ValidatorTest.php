895c8ab736d23278c2fd6f4664922f4af8ef4057
Falaleev Maxim <max@studio107.ru>
2014-10-21 15:29:23 +0400

    tests

---
+++ Tests/Cases/Validation/ValidatorTest.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * 
+ *
+ * All rights reserved.
+ * 
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 21/10/14.10.2014 13:26
+ */
+
+namespace Mindy\Validation\Tests;
+
+use Mindy\Helper\Creator;
+use Mindy\Helper\Traits\Accessors;
+use Mindy\Helper\Traits\Configurator;
+use Mindy\Validation\Interfaces\IValidateField;
+use Mindy\Validation\Interfaces\IValidateObject;
+use Mindy\Validation\RequiredValidator;
+use Mindy\Validation\Traits\ValidateField;
+use Mindy\Validation\Traits\ValidateObject;
+
+class Field implements IValidateField
+{
+    use Accessors, Configurator, ValidateField;
+
+    /**
+     * @var
+     */
+    private $_value;
+
+    /**
+     * @param $value
+     * @return $this
+     */
+    public function setValue($value)
+    {
+        $this->_value = $value;
+        return $this;
+    }
+
+    /**
+     * @return mixed the value for validation
+     */
+    public function getValue()
+    {
+        return $this->_value;
+    }
+}
+
+class Form implements IValidateObject
+{
+    use Accessors, Configurator, ValidateObject;
+
+    /**
+     * @var array
+     */
+    public $fields = [];
+    /**
+     * @var array
+     */
+    private $_fields = [];
+
+    public function init()
+    {
+        foreach($this->fields as $name => $params) {
+            $this->_fields[$name] = Creator::createObject($params);
+        }
+    }
+
+    /**
+     * @return mixed the initialized fields for validation
+     */
+    public function getFieldsInit()
+    {
+        return $this->_fields;
+    }
+
+    /**
+     * @param $attribute string
+     * @return bool check the field isset
+     */
+    public function hasField($attribute)
+    {
+        return array_key_exists($attribute, $this->_fields);
+    }
+
+    /**
+     * @param $attribute string
+     * @return object field instance
+     */
+    public function getField($attribute)
+    {
+        return $this->_fields[$attribute];
+    }
+}
+
+class CleanForm extends Form
+{
+    public function cleanName($value)
+    {
+        if(empty($value)) {
+            $this->addError('name', 'Error');
+        } else {
+            return '1' . $value;
+        }
+    }
+}
+
+class ValidatorTest extends TestCase
+{
+    public function testValidation()
+    {
+        $v = new RequiredValidator;
+        $v->setName('foo');
+        $this->assertEquals('foo', $v->getName());
+    }
+
+    public function testValidationField()
+    {
+        $f = new Field([
+            'validators' => [
+                new RequiredValidator
+            ]
+        ]);
+
+        $this->assertEquals(1, count($f->validators));
+        $this->assertFalse($f->isValid());
+        $this->assertTrue($f->hasErrors());
+        $this->assertEquals(['Value cannot be empty'], $f->getErrors());
+        $f->clearErrors();
+        $this->assertFalse($f->hasErrors());
+        $this->assertEquals([], $f->getErrors());
+        $f->setValue(1);
+        $this->assertTrue($f->isValid());
+
+        $f->addError('foo');
+        $this->assertTrue($f->hasErrors());
+        $this->assertEquals(['foo'], $f->getErrors());
+        $this->assertTrue($f->isValid());
+
+        $f = new Field([
+            'validators' => [
+                function($value) {
+                    return 'Error';
+                }
+            ],
+        ]);
+        $this->assertFalse($f->isValid());
+        $this->assertTrue($f->hasErrors());
+        $this->assertEquals(['Error'], $f->getErrors());
+    }
+
+    public function testValidationObject()
+    {
+        $f = new Form([
+            'fields' => [
+                'name' => [
+                    'class' => Field::className(),
+                    'validators' => [
+                        new RequiredValidator
+                    ]
+                ]
+            ],
+        ]);
+
+        $this->assertEquals(1, count($f->getFieldsInit()));
+        $this->assertTrue($f->hasField('name'));
+        $this->assertInstanceOf(Field::className(), $f->getField('name'));
+        $this->assertFalse($f->isValid());
+        $this->assertEquals(['name' => ['Value cannot be empty']], $f->getErrors());
+        $this->assertEquals(['Value cannot be empty'], $f->getErrors('name'));
+        $this->assertTrue($f->hasErrors());
+        $f->clearErrors();
+        $this->assertFalse($f->hasErrors());
+
+        $this->assertFalse($f->isValid());
+        $f->clearErrors('name');
+        $this->assertFalse($f->hasErrors());
+    }
+
+    public function testCleanData()
+    {
+        $f = new CleanForm([
+            'fields' => [
+                'name' => [
+                    'class' => Field::className(),
+                    'validators' => [
+                        new RequiredValidator
+                    ]
+                ]
+            ],
+        ]);
+        $this->assertFalse($f->isValid());
+        $this->assertEquals(['name' => ['Error', 'Value cannot be empty']], $f->getErrors());
+        $this->assertEquals(['Error', 'Value cannot be empty'], $f->getErrors('name'));
+
+        $f->getField('name')->setValue(1);
+        $this->assertTrue($f->isValid());
+    }
+}
---
+++ Tests/Cases/Validation/ValidatorsTest.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * 
+ *
+ * All rights reserved.
+ * 
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 21/10/14.10.2014 13:55
+ */
+
+namespace Mindy\Validation\Tests;
+
+use Mindy\Helper\Interfaces\Arrayable;
+use Mindy\Validation\DateValidator;
+use Mindy\Validation\EmailValidator;
+use Mindy\Validation\FileValidator;
+use Mindy\Validation\IpValidator;
+use Mindy\Validation\JsonValidator;
+use Mindy\Validation\MaxLengthValidator;
+use Mindy\Validation\MinLengthValidator;
+use Mindy\Validation\UrlValidator;
+
+class ValidatorsTest extends TestCase
+{
+    public function testMaxLengthValidator()
+    {
+        $v = new MaxLengthValidator(2);
+        $this->assertEquals(2, $v->maxLength);
+        $this->assertTrue($v->validate(1));
+        $this->assertEquals([], $v->getErrors());
+        $v->clearErrors();
+        $this->assertFalse($v->validate(123));
+        $this->assertEquals(['Maximum length is 2'], $v->getErrors());
+
+        $v->clearErrors();
+        $obj = new \StdClass;
+        $this->assertTrue(!is_numeric($obj) && !is_string($obj));
+        $this->assertFalse($v->validate($obj));
+        $this->assertEquals(['object is not a string'], $v->getErrors());
+    }
+
+    public function testMinLengthValidator()
+    {
+        $v = new MinLengthValidator(2);
+        $this->assertEquals(2, $v->minLength);
+        $this->assertFalse($v->validate(1));
+        $this->assertEquals(['Minimal length is 2'], $v->getErrors());
+        $v->clearErrors();
+        $this->assertTrue($v->validate(123));
+        $this->assertEquals([], $v->getErrors());
+
+        $v->clearErrors();
+        $obj = new \StdClass;
+        $this->assertTrue(!is_numeric($obj) && !is_string($obj));
+        $this->assertFalse($v->validate($obj));
+        $this->assertEquals(['object is not a string'], $v->getErrors());
+    }
+
+    public function testEmailValidator()
+    {
+        $v = new EmailValidator();
+        $this->assertFalse($v->validate('admin@admin'));
+        $this->assertEquals(['Is not a valid email address'], $v->getErrors());
+        $v->clearErrors();
+
+        $this->assertTrue($v->validate('admin@admin.com'));
+        $this->assertEquals([], $v->getErrors());
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate(''));
+        $this->assertEquals(['Is not a valid email address'], $v->getErrors());
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate(new \StdClass));
+        $this->assertEquals(['Is not a valid email address'], $v->getErrors());
+        $v->clearErrors();
+
+        $v->checkDNS = true;
+        $this->assertFalse($v->validate('qwe@foo.qwe'));
+        $this->assertEquals(['Is not a valid email address'], $v->getErrors());
+        $v->clearErrors();
+
+        $v->checkDNS = false;
+        $v->enableIDN = true;
+        $this->setExpectedException('Mindy\Exception\InvalidConfigException');
+        $this->assertFalse($v->validate('qwe@foo.qwe'));
+        $this->assertEquals(['Is not a valid email address'], $v->getErrors());
+        $v->clearErrors();
+    }
+
+    public function testUrlValidator()
+    {
+        $v = new UrlValidator();
+        $this->assertTrue($v->validate('http://studio107.ru'));
+        $v->clearErrors();
+
+        $this->assertTrue($v->validate('ftp://studio107.ru'));
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate('/foo/bar.html'));
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate('qwe'));
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate('studio107.ru'));
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate(new \StdClass));
+        $v->clearErrors();
+
+        $v->pattern = '/^(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)/i';
+        $this->assertTrue($v->validate('studio107.ru'));
+        $v->clearErrors();
+
+        $v->enableIDN = true;
+        $this->setExpectedException('Mindy\Exception\InvalidConfigException');
+        $this->assertTrue($v->validate('studio107.ru'));
+        $v->clearErrors();
+    }
+
+    public function testDateValidator()
+    {
+        $v = new DateValidator('Y-m-d');
+        $this->assertTrue($v->validate('2012-10-11'));
+        $v->clearErrors();
+
+        $v = new DateValidator();
+        $this->assertFalse($v->validate('2012-11-10 00:00:00'));
+        $v->clearErrors();
+
+        $this->assertTrue($v->validate('2012-10-11'));
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate('2012-11-10 00:00:00'));
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate(new \StdClass));
+        $v->clearErrors();
+    }
+
+    public function testJsonValidator()
+    {
+        $v = new JsonValidator();
+
+        $this->assertTrue($v->validate([]));
+        $v->clearErrors();
+
+        $this->assertTrue($v->validate(null));
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate(new \StdClass));
+        $this->assertEquals(['Not json serialize object: object'], $v->getErrors());
+        $v->clearErrors();
+
+        $obj = new Arr;
+        $obj->data = [1, 2, 3];
+        $this->assertTrue($v->validate($obj));
+        $v->clearErrors();
+    }
+
+    public function testFileValidator()
+    {
+        $v = new FileValidator(['txt']);
+
+        $this->assertTrue($v->validate(['name' => 'test.txt']));
+        $v->clearErrors();
+
+        $this->assertFalse($v->validate(['name' => 'test.mp3']));
+        $this->assertEquals(['Is not a valid file type mp3. Types allowed: txt'], $v->getErrors());
+        $v->clearErrors();
+    }
+
+    public function testIpValidator()
+    {
+        $v = new IpValidator();
+        $this->assertTrue($v->validate('127.0.0.1'));
+        $this->assertTrue($v->validate('8.8.8.8'));
+        $this->assertFalse($v->validate('8.8.8'));
+        $this->assertEquals(['Is not a valid IP address.'], $v->getErrors());
+    }
+}
+
+class Arr implements Arrayable
+{
+    public $data = [];
+
+    public function toArray()
+    {
+        return $this->data;
+    }
+}
