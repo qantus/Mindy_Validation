6bb088bef8b88d1fe255aa1f61116af4cc023108
Falaleev Maxim <max@studio107.ru>
2014-10-26 19:22:57 +0300

    Fix multiple errors in field

--- src/Mindy/Validation/Traits/ValidateField.php
+++ src/Mindy/Validation/Traits/ValidateField.php
@@ -50,7 +50,7 @@ trait ValidateField
                     $this->addErrors($valid);
                 }
             } else if ($validator instanceof Validator) {
-                if($this instanceof \Mindy\Form\Fields\Field) {
+                if($this->form instanceof \Mindy\Form\ModelForm) {
                     $validator->setModel($this->form->getInstance());
                 }
                 $validator->clearErrors();
@@ -65,7 +65,7 @@ trait ValidateField
 
     public function getErrors()
     {
-        return $this->_errors;
+        return array_unique($this->_errors);
     }
 
     public function hasErrors()
